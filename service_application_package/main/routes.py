7cc26d519b56642327e1b3e9d2bb9e06ae87dcd9
Matthew Tse <tsemd@bu.edu>
2019-10-10 09:20:10 -0400

    simplified routes

--- service_application_package/main/routes.py
+++ service_application_package/main/routes.py
@@ -1,20 +1,17 @@
 from flask import render_template, request, Blueprint
-# from wtforms import IntegerField
 from service_application_package.main.forms import GenerationNumberAndStartForm
 
 main = Blueprint('main', __name__)
 
-
 @main.route("/")
-@main.route("/home")
-def home():
+@main.route("/geneticModel/<int:numberOfGenerations>")
+def home(numberOfGenerations):
 	
 	form = GenerationNumberAndStartForm()
-	# return render_template('home.html')
-	return render_template('home.html', nog=form.numberOfGenerations)
-@main.route("/geneticModel/<int:numberOfGenerations>")
-def start(numberOfGenerations):
-	form = GenerationNumberAndStartForm()
-	return render_template('geneticModel.html', numberOfGenerations=form.numberOfGenerations, form=form)
+
+	return render_template('home.html', numberOfGenerations=form.numberOfGenerations, form=form)
+# @main.route("/geneticModel/<int:numberOfGenerations>")
+# def start(numberOfGenerations):
+	# create start function
 # def stop():
 	# create stop function
