5b7f05a18ea80d45722ed8e237e3faa17a06f871
Matthew D Tse <tsemd@bu.edu>
2019-10-16 20:58:00 -0400

    elk

--- service_application_package/main/elk.py
+++ service_application_package/main/elk.py
@@ -1,1 +1,8 @@
-Elk
+class Elk:
+	def __init__(self, x0):
+		self.position_i=[]
+		self.velocity_i=[]
+		self.pos_best_i=[]
+		self.err_best_i=-1
+		self.err_i=-1
+		# pip freeze | xargs pip uninstall -y
