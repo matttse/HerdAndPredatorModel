f1415ff96b6e3701726d2f7f75598d311535585f
Matthew D Tse <tsemd@bu.edu>
2019-10-22 11:50:21 -0400

    successful matlabplot on flask

--- service_application_package/main/routes.py
+++ service_application_package/main/routes.py
@@ -1,5 +1,8 @@
 from flask import render_template, request, Blueprint
 from service_application_package.main.forms import GenerationNumberAndStartForm
+import matplotlib.pyplot as plt
+import io
+import base64
 
 main = Blueprint('main', __name__)
 
@@ -14,7 +17,17 @@ def home(numberOfGenerations):
 def start():
 	# create start function
 	form = GenerationNumberAndStartForm()
+	img = io.BytesIO()
 
-	return render_template('geneticModel.html', numberOfGenerations=form.numberOfGenerations)
+	y = [1,2,3,4,5]
+	x = [0,2,1,3,4]
+
+	plt.plot(x,y)
+	plt.savefig(img, format='png')
+	img.seek(0)
+
+	plot_url = base64.b64encode(img.getvalue()).decode()
+	return '<img src="data:image/png;base64,{}">'.format(plot_url)
+	# return render_template('geneticModel.html', numberOfGenerations=form.numberOfGenerations)
 # def stop():
 	# create stop function
