0482b762740cc3c7d83e6d2bd074f63d56f0113a
Matthew D Tse <tsemd@bu.edu>
2019-11-03 20:48:54 -0500

    dna methods and import

--- service_application_package/static/dna.js
+++ service_application_package/static/dna.js
@@ -10,5 +10,30 @@ function DNA(genes) {
 		}
 	}
 
-	// this.mating
+	this.mating = function(mate) {
+		var childDNA = [];
+		//randomly select via p5 library
+		//a point that is somewhere in the middle
+		var midPoint = floor(random(this.genes.length));
+		//create new / overwrite DNA with parents
+		for (var i = 0; i < this.genes.length; i++) {
+			if (i > midPoint) {
+				childDNA[i] = this.genes[i];
+			} else {
+				childDNA[i] = mate.genes[i];
+			}
+
+		}
+		return new DNA(childDNA);
+	}
+	//allows for some variability rather than just the first generation genes
+	this.mutation = function() {
+		for (var i = 0; i < this.genes.length; i++) {
+			if (random(1) < 0.01) {//random number with mutation rate of 1%
+				this.genes[i] = p5.Vector.random2D();//becomes new Random vector
+				this.genes[i].setMag(0.1); //length of vector
+			}
+			
+		}
+	}
 }
